73555de0417695c02fb144f0c6fe1859d0b96ea0
lucas <schneider.lusca@gmail.com>
2023-04-20 20:45:05 -0300

    Initial commit

--- Client.java
+++ Client.java
@@ -7,8 +7,12 @@ public class Client {
     public static void main(String argv[]) throws Exception {
         
         // verifica se o jogador ja jogou
-        boolean[] settings = new boolean[1];
+        boolean[] settings = new boolean[2];
         settings[0] = false;
+        settings[1] = false;
+
+        // int[] saldo = new int[1];
+        // saldo[0] =  1000;
 
         BufferedReader readerTeclado = new BufferedReader(new InputStreamReader(System.in)); // le o teclado
         Socket socket = new Socket("127.0.0.1", 6789); // socket do jogador
@@ -22,6 +26,14 @@ public class Client {
                 String retornoServidor;
                 while ((retornoServidor = reader.readLine()) != null) {
                     System.out.println("Resultado da rodada: " + retornoServidor);
+
+                    // synchronized(saldo) {
+                    //     if(retornoServidor.contains("ganhou")) {
+                    //         saldo[0] = saldo[0] + Integer.parseInt(retornoServidor.replace("Voce ganhou ", ""));
+                    //     }
+                    //     System.out.println("Seu saldo de fichas: " + saldo[0]);
+                    // }
+
                     synchronized(settings) {
                         settings[0] = false;
                     }
@@ -35,16 +47,37 @@ public class Client {
         while (true) { 
             synchronized(settings) { // garante o controle de concorrencia
                 if(settings[0] == false) { // verifica se o jogador ja jogou
-                    System.out.print("\n----- Nova rodada -----\n");
-                    System.out.print("Qual a sua jogada (numero): ");
+                    
 
-                    String jogada = readerTeclado.readLine(); // le a jogada do teclado
 
-                    sender.writeBytes(jogada + '\n'); // envia a jogada para o servidor
+                    if(settings[1] == false) {
 
-                    settings[0] = true; // atualiza o controle de jogadas
+                        System.out.print("\n----- Nova rodada -----\n");
 
-                    System.out.print("Aguardando o giro da roleta...\n");
+                        settings[1] = true;
+
+                        System.out.print("Quantas fichas: ");
+
+                        String fichas = readerTeclado.readLine();
+
+                        //saldo[0] = saldo[0] - Integer.parseInt(fichas);
+
+                        sender.writeBytes(fichas +" fichas"+ '\n');
+
+                    } else {
+
+                        settings[1] = false;
+
+                        System.out.print("Qual a sua jogada (numero): ");
+
+                        String jogada = readerTeclado.readLine(); // le a jogada do teclado
+
+                        sender.writeBytes(jogada + '\n'); // envia a jogada para o servidor
+
+                        settings[0] = true; // atualiza o controle de jogadas
+
+                        System.out.print("Aguardando o giro da roleta...\n");
+                    }
                 }
             }
         }
--- Server.java
+++ Server.java
@@ -16,6 +16,11 @@ class Server {
         settings[1] = 0; //jogadas feitas
 
         int[] jogadas = new int[2]; // armazena as jogadas 
+        int[] fichas = new int[2]; // fichas apostadas na rodada
+
+        int[] saldo = new int[2];
+        saldo[0] = 1000;
+        saldo[1] = 1000;
   
         while (true) {
 
@@ -23,8 +28,9 @@ class Server {
             
             conexoes.add(socketConexao); // adiciona esse socket de uma nova conexao em uma lista
 
-            settings[0] = settings[0] + 1; // incrementa o numero de conexoes
-            
+            synchronized(settings) {
+                settings[0] = settings[0] + 1; // incrementa o numero de conexoes
+            }
             // cria uma nova thread para cada jogador
             // isso vai garantir que cada conexao tenha uma thread para si propria, garantindo multiplas conexoes
             new Thread(() -> {
@@ -36,10 +42,55 @@ class Server {
 
                         String jogada = doCliente.readLine(); // le a jogada do jogador
                         
+
+
+                        if(jogada.contains("fichas")) {
+                        
+                            synchronized(jogadas) {
+
+                                int aposta = Integer.parseInt(jogada.replace(" fichas", ""));
+                                
+                                // salva a jogada 
+                                if(conexoes.get(0) == socketConexao) {
+                                    fichas[0] = aposta;
+                                    saldo[0] = saldo[0] - aposta;
+                                } else {
+                                    fichas[1] = aposta;
+                                    saldo[1] = saldo[1] - aposta;
+                                }
+                                 
+                            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        } else {
+
+
+
+
                         synchronized(settings) { // garante o controle de concorrencia entre threads (todo synchronized faz isso com variaves globais) 
                             
                             synchronized(jogadas) {
-                                jogadas[settings[0] - 1] = Integer.parseInt(jogada); // armazena a jogada no array de jogadas
+                                
+                                // salva a jogada 
+                                if(conexoes.get(0) == socketConexao) {
+                                    jogadas[0] = Integer.parseInt(jogada);
+                                } else {
+                                    jogadas[1] = Integer.parseInt(jogada);
+                                }
+                                 
                             }
 
                             settings[1] = settings[1] + 1; // incrementa o numero de jogadas feitas
@@ -49,6 +100,7 @@ class Server {
                                 // gira a roleta
                                 Random random = new Random();
                                 int numeroRoleta = random.nextInt(36) + 1;
+                                //int numeroRoleta = 10;
 
                                 synchronized (conexoes) {
                                     
@@ -59,27 +111,44 @@ class Server {
                                         String resultado;
                                         
                                         // verifica se o jogador atual ganhou
-                                        if(numeroRoleta == jogadas[aux]){
-                                            resultado = "Voce ganhou!\n";
+                                        if(numeroRoleta == jogadas[aux]) {
+                                            saldo[aux] = saldo[aux] + fichas[aux]*2;
+                                            resultado = "Voce ganhou "+saldo[aux]+" fichas, seu saldo atual: "+saldo[aux]+'\n';
                                         } else {
-                                            resultado = "Voce perdeu :c | Numero ganhador: "+numeroRoleta+"\n";
+                                            resultado = "Voce perdeu :c | Numero ganhador: "+numeroRoleta+" | Voce jogou: "+jogadas[aux]+" | Voce perdeu: "+fichas[aux]+" fichas, seu saldo atual: "+saldo[aux]+"\n";
                                         }
                                         
-                                        // reseta as jogadas para uma nova rodada
-                                        settings[1] = 0;
-                                        jogadas[0] = 0;
-                                        jogadas[1] = 0;
+                                        System.out.println(jogadas[0]);
+                                        System.out.println(jogadas[1]);
+                                        aux = aux + 1;
+                                        
+                                        
 
                                         // envia o resultado da rodada para os jogadores
                                         DataOutputStream sender = new DataOutputStream(conexao.getOutputStream());
                                         sender.writeBytes(resultado);
                                     }
+
+                                    // reseta as jogadas para uma nova rodada
+                                    settings[1] = 0;
+                                    jogadas[0] = 0;
+                                    jogadas[1] = 0;
+
                                 }
                             }
 
                         }
 
                             
+
+                        
+                    }
+
+
+
+
+
+
                         
                     }
                 } catch (IOException e) {
